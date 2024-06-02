f9ee7d87aea172b250e919e3453ad94139d45a2a
udanthaaa <damikaudantha@gmail.com>
2024-06-02 12:50:03 +0530

    Added some css and Changed CV form

--- src/components/CVList.js
+++ src/components/CVList.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+// CVList.js
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Styles/CVList.css';
 
