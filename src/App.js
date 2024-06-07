70414234dfa85bc22a4dd00adcea6168dec69ff8
udanthaaa <damikaudantha@gmail.com>
2024-06-07 22:29:41 +0530

    Errors Fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import CVForm from './components/CVForm';
@@ -9,18 +9,16 @@ import CVView from './components/CVView';
 import CVCreation from './components/CVCreation';
 
 const App = () => {
-  const handleFormSubmit = (data) => {
-    console.log('Form submitted:', data);
-  };
 
   return (
     <Router>
       <div>
         <Navbar />
         <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/cv-creation" element={<CVCreation />} />
-          <Route path="/cv-form" element={<CVForm onSubmit={handleFormSubmit} />} />
+          <Route path="/cv-form" element={<CVForm />} />
           <Route path="/cv-list" element={<CVList />} />
           <Route path="/cv-view/:id" element={<CVView />} />
           <Route path="/cv-edit/:id" element={<CVEdit />} />
